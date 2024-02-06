26d45c67bf33a7b0b6e1ef696c9fcd8adbbb219e
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-02-06 19:55:40 -0300

    finalizado

--- aula12ex/ex15/interacoes/arquivo.js
+++ aula12ex/ex15/interacoes/arquivo.js
@@ -18,23 +18,46 @@ function verificar() {
             genero = 'Homem'
             if (idade >=0 && idade < 10){
                 //criança
+                img.setAttribute('src', 'imagens/bebe-m.jpg')
             } else {
                 if (idade < 21) {
                 //jovem
+                img.setAttribute('src', 'imagens/jovem-m2.jpg')
             } else {
                 if (idade < 50) {
                 //adulto
+                img.setAttribute('src', 'imagens/adulto-2.jpg')
             } else {
                 //idoso
+                img.setAttribute('src', 'imagens/idoso.jpg')
                 }
             }
             }
 
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >=0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'imagens/bebe-f.jpg')
+            } else {
+                if (idade < 21) {
+                    //jovem
+                    img.setAttribute('src', 'imagens/jovem-f.jpg')
+                } else {
+                    if (idade < 50) {
+                        //adulta
+                        img.setAttribute('src', 'imagens/adulta.jpg')
+                    } else {
+                        //idoso
+                        img.setAttribute('src', 'imagens/idosa.jpg')
+                    }
+                }
+            }
+
         }
-        
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+        
     }
 }
