ffd46e635c90fe67b5f9ccc8733f540813f52c22
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-02-08 16:40:09 -0300

    js

--- PRATICA/EXERCICIO 15/interaction/archive.js
+++ PRATICA/EXERCICIO 15/interaction/archive.js
@@ -1,15 +1,9 @@
-/* CRIANÇA M, F
-JOVEM M, F
-ADULTO M , F
-IDOSO M , F
-*/
-
 function verify() {
     var date = new Date()
     var year = date.getFullYear()
 
     let fyear = document.getElementById('txty')
-    let res = document.getElementById('res')
+    let res = document.querySelector('div#res')
 
     if (fyear.value.length == 0 || Number(fyear.value) > year) {
         res.innerHTML = 'ERRO! Verifique e preencha os dados corretamente e tente novamente!'
@@ -23,15 +17,46 @@ function verify() {
         
         if (fsex[0].checked) {
             gender = 'Homem'
+            // CRIANÇA
             if (yo >= 0 && yo < 13) {
-                img.src = 'images/child-m.jpg'
                 img.setAttribute('src', 'images/child-m.jpg')
             } else {
-                if (yo < 18)
-                img.setAttribute('src', 'images/young.jpg')
+                // JOVEM
+                if (yo < 18) {
+                img.setAttribute('src', 'images/young-m.jpg')
+            } else {
+                // ADULTO
+                if (yo < 59) {
+                    img.setAttribute('src', 'images/adult-m.jpg')
+                } else {
+                    // IDOSO
+                    img.setAttribute('src', 'images/old-m.jpg')
+                }
+            }
+            }
+        } else {
+            if (fsex[1].checked) {
+                gender = 'Mulher'
+                // CRIANÇA
+                if (yo >= 0 && yo < 13) {
+                    img.setAttribute('src', 'images/child-f.jpg')
+                } else {
+                    // JOVEM 
+                    if (yo < 18) {
+                        img.setAttribute('src', 'images/young-f.jpg')
+                    } else {
+                        // ADULTA 
+                        if (yo < 59) {
+                            img.setAttribute('src', 'images/adult-f.jpg')
+                        } else {
+                            // IDOSO
+                            img.setAttribute('src', 'images/old-f.jpg')
+                        }
+                    }
+                }
             }
         }
+        res.innerHTML = `Detectamos <strong>${gender} com ${yo} anos.</strong>`
         res.appendChild(img)
-        res.innerHTML = `Detectamos ${gender} com ${yo} anos.`
     }
 }
