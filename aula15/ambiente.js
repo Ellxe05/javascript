037f76d08f5a43899029fccb779693f9f749155a
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-02-09 10:36:34 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -4,3 +4,10 @@ num.sort()
 console.log(num)
 console.log(`Nosso vetor tem ${num.length} posições.`)
 console.log(`O primeiro valor do vetor é ${num[0]}.`)
+
+let pos = num.indexOf(3)
+if (pos == -1) {
+    console.log('ERRO! Esse valor não foi encontrado, tente novamente.')
+} else {
+    console.log(`O valor ${num[pos]} está na posição ${pos}`)
+}
